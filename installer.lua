6039567c8cb04f0366ed27e861840b2f4f3b5610
SethGamer1223 <155033256+SethGamer1223@users.noreply.github.com>
2026-06-06 23:28:49 -0400

    oop

--- installer.lua
+++ installer.lua
@@ -2,7 +2,7 @@ shell.run("wget https://raw.githubusercontent.com/Twijn/cc-misc/main/util/cmd.lu
 shell.run("wget https://raw.githubusercontent.com/Twijn/cc-misc/main/util/pager.lua")
 shell.run("wget https://raw.githubusercontent.com/Kromify-rcc/shipifyDocumentation/main/shipifyLib.lua")
 shell.run("wget https://raw.githubusercontent.com/Kromify-rcc/shipifyDocumentation/main/shipifycli.lua")
-shell.run("wget https://github.com/migeyel/ccryptolib/releases/download/v1.2.2/install.lua")
-shell.run("wget https://github.com/migeyel/ecnet/releases/download/v2.1.0/install.lua")
+shell.run("wget runhttps://github.com/migeyel/ccryptolib/releases/download/v1.2.2/install.lua")
+shell.run("wget run https://github.com/migeyel/ecnet/releases/download/v2.1.0/install.lua")
 
 print("Shipify CLI has been installed! run shipifycli.lua")
