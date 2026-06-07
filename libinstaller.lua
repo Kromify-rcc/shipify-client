430bf4b17a79d3d853dba81a9f13da340db2aa00
SethGamer1223 <155033256+SethGamer1223@users.noreply.github.com>
2026-06-07 15:08:47 -0400

    create libinstaller
    created a installer to only install lib

---
+++ libinstaller.lua
@@ -0,0 +1,5 @@
+shell.run("wget https://raw.githubusercontent.com/Kromify-rcc/shipify-client/main/shipifyLib.lua")
+shell.run("wget run https://github.com/migeyel/ccryptolib/releases/download/v1.2.2/install.lua")
+shell.run("wget run https://github.com/migeyel/ecnet/releases/download/v2.1.0/install.lua")
+
+print("Shipify lib has been installed!")
