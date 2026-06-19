eda99bde894a17f254f575b57e1551ad79481ae3
SethGamer1223 <155033256+SethGamer1223@users.noreply.github.com>
2026-06-18 23:27:41 -0400

    Make installer smarter.
    Installer will now try and update shipify scripts instead of just fetching them.

--- installer.lua
+++ installer.lua
@@ -1,8 +1,36 @@
-shell.run("wget https://raw.githubusercontent.com/Twijn/cc-misc/main/util/cmd.lua")
-shell.run("wget https://raw.githubusercontent.com/Twijn/cc-misc/main/util/pager.lua")
-shell.run("wget https://raw.githubusercontent.com/Kromify-rcc/shipify-client/main/shipifyLib.lua")
-shell.run("wget https://raw.githubusercontent.com/Kromify-rcc/shipify-client/main/shipifycli.lua")
-shell.run("wget run https://github.com/migeyel/ccryptolib/releases/download/v1.2.2/install.lua")
-shell.run("wget run https://github.com/migeyel/ecnet/releases/download/v2.1.0/install.lua")
+local function installFile(name, url, force)
+    if force and fs.exists(name) then
+        fs.delete(name)
+    end
 
+    if not fs.exists(name) then
+        shell.run("wget", url, name)
+    end
+end
+
+local function installScript(dir, url)
+    if not fs.exists(dir) then
+        shell.run("wget", "run", url)
+    end
+end
+
+installFile("shipifyLib.lua",
+    "https://raw.githubusercontent.com/Kromify-rcc/shipify-client/main/shipifyLib.lua",
+    true)
+
+installFile("shipifycli.lua",
+    "https://raw.githubusercontent.com/Kromify-rcc/shipify-client/main/shipifycli.lua",
+    true)
+
+installFile("cmd.lua",
+    "https://raw.githubusercontent.com/Twijn/cc-misc/main/util/cmd.lua")
+
+installFile("pager.lua",
+    "https://raw.githubusercontent.com/Twijn/cc-misc/main/util/pager.lua")
+
+installScript("ccryptolib",
+    "https://github.com/migeyel/ccryptolib/releases/download/v1.2.2/install.lua")
+
+installScript("ecnet2",
+    "https://github.com/migeyel/ecnet/releases/download/v2.1.0/install.lua")
 print("Shipify CLI has been installed! run shipifycli.lua")
--- libinstaller.lua
+++ libinstaller.lua
@@ -1,5 +1,28 @@
-shell.run("wget https://raw.githubusercontent.com/Kromify-rcc/shipify-client/main/shipifyLib.lua")
-shell.run("wget run https://github.com/migeyel/ccryptolib/releases/download/v1.2.2/install.lua")
-shell.run("wget run https://github.com/migeyel/ecnet/releases/download/v2.1.0/install.lua")
+local function installFile(name, url, force)
+    if force and fs.exists(name) then
+        fs.delete(name)
+    end
+
+    if not fs.exists(name) then
+        shell.run("wget", url, name)
+    end
+end
+
+local function installScript(dir, url)
+    if not fs.exists(dir) then
+        shell.run("wget", "run", url)
+    end
+end
+
+
+installFile("shipifyLib.lua",
+    "https://raw.githubusercontent.com/Kromify-rcc/shipify-client/main/shipifyLib.lua",
+    true)
+
+installScript("ccryptolib",
+    "https://github.com/migeyel/ccryptolib/releases/download/v1.2.2/install.lua")
+
+installScript("ecnet2",
+    "https://github.com/migeyel/ecnet/releases/download/v2.1.0/install.lua")
 
 print("Shipify lib has been installed!")
